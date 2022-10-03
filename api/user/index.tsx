0c7cf763871c39cdf04b4c0cbc3ca69e7d0df28d
경민 <a01098956117@gmail.com>
2022-10-03 16:18:48 +0900

    create getMe API function

--- api/user/index.tsx
+++ api/user/index.tsx
@@ -1,6 +1,10 @@
-import { post } from '../axios';
+import { get, post } from '../axios';
 import { LoginDto } from './dto/login-request.dto';
 
 export const login = (loginDto: LoginDto) => {
   return post('auth/login', loginDto);
+};
+
+export const getMe = () => {
+  return get('auth/me');
 };
