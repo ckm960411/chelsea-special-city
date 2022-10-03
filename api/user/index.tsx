4225b1a87dfbadaad927f4bb6c553802ef4f976c
경민 <a01098956117@gmail.com>
2022-10-03 17:10:20 +0900

    create sign up API function

--- api/user/index.tsx
+++ api/user/index.tsx
@@ -1,8 +1,13 @@
 import { get, post } from '../axios';
 import { LoginDto } from './dto/login-request.dto';
+import { SignUpDto } from './dto/signup-request.dto';
 
 export const login = (loginDto: LoginDto) => {
   return post('auth/login', loginDto);
+};
+
+export const signUp = (signUpDto: SignUpDto) => {
+  return post('auth/signup', signUpDto);
 };
 
 export const getMe = () => {
