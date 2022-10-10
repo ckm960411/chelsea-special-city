095e4d5eba4dac61fc21b78cd6822dde1a8e2d65
경민 <a01098956117@gmail.com>
2022-10-10 21:11:38 +0900

    Create CreateCommentForm

--- components/player/PlayerDetailComments.tsx
+++ components/player/PlayerDetailComments.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react';
-import ReactTextareaAutosize from 'react-textarea-autosize';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { getPlayerComments } from '../../api/players';
+import PlayerCommentForm from './PlayerCommentForm';
 
 const PlayerDetailComments = () => {
-  const [comment, setComment] = useState('');
+  const router = useRouter();
+  const { id: playerName } = router.query;
+
+  useEffect(() => {
+    playerName &&
+      getPlayerComments(playerName as string)
+        .then((res) => console.log(res.data))
+        .catch(() => {});
+  }, [playerName]);
 
   return (
-    <div className="h-full px-16px pt-16px">
-      <ReactTextareaAutosize
-        id="create-player-detail-comment"
-        placeholder="write your comment!"
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-        minRows={2}
-        className="w-full rounded-sm border border-chelsea px-12px py-6px"
-        style={{ outline: 'none', resize: 'none' }}
-      />
-      <div className="flex items-center justify-end gap-8px font-medium">
-        <button
-          onClick={() => setComment('')}
-          className="rounded-sm border border-chelsea px-8px py-4px text-chelsea"
-        >
-          Cancel
-        </button>
-        <button
-          onClick={() => alert(`comment: ${comment}`)}
-          className="rounded-sm border border-chelsea bg-chelsea px-8px py-4px text-white"
-        >
-          Send
-        </button>
-      </div>
+    <div>
+      <PlayerCommentForm />
     </div>
   );
 };
