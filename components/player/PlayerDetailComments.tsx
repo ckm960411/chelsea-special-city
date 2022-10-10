7b8f979593eef81001105601bf6fb67870e11da5
ckm960411 <a01098956117@gmail.com>
2022-10-10 16:41:08 +0900

    Create comment form

--- components/player/PlayerDetailComments.tsx
+++ components/player/PlayerDetailComments.tsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+import ReactTextareaAutosize from 'react-textarea-autosize';
+
 const PlayerDetailComments = () => {
-  return <div className="h-full">PlayerDetailComments</div>;
+  const [comment, setComment] = useState('');
+
+  return (
+    <div className="h-full px-16px pt-16px">
+      <ReactTextareaAutosize
+        id="create-player-detail-comment"
+        placeholder="write your comment!"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        autoFocus
+        minRows={2}
+        className="w-full rounded-sm border border-chelsea px-12px py-6px"
+        style={{ outline: 'none', resize: 'none' }}
+      />
+      <div className="flex items-center justify-end gap-8px font-medium">
+        <button
+          onClick={() => setComment('')}
+          className="rounded-sm border border-chelsea px-8px py-4px text-chelsea"
+        >
+          Cancel
+        </button>
+        <button
+          onClick={() => alert('comment: ', comment)}
+          className="rounded-sm border border-chelsea bg-chelsea px-8px py-4px text-white"
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PlayerDetailComments;
